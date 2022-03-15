8388a0828345096f082bea608bb56dca6fc04e09
abybells <abbiolbello@yahoo.com>
2022-03-15 14:46:33 -0700

    100-times_table.c added

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -24,8 +24,8 @@ void print_times_table(int n)
 if (op <= 9)
 {
 _putchar(32);
-_putchar((op / 10) + 48);
-_putchar((op % 10) + 48);
+_putchar(32);
+_putchar(op + 48);
 }
 else if (op <= 99)
 {
@@ -36,8 +36,8 @@ else if (op <= 99)
 else
 {
 	_putchar(((op / 100) % 10) + 48);
-	_putchar(((op / 10) % 10) +48);
-	_putchar((op % 10) +48);
+	_putchar(((op / 10) % 10) + 48);
+	_putchar((op % 10) + 48);
 }
 }
 _putchar('\n');

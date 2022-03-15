aab0ec94ecd5399729ff566262f43ddeaea85bf0
abybells <99332371+abybells@users.noreply.github.com>
2022-03-15 18:41:53 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * -sign.c - Determines if the input number is
+ * print_sign - Determines if the input number is
  *  reater, equal or less than zero.
  *
  *  @n: The input number as an integer.

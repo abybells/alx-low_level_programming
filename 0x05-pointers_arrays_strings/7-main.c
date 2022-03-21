11263552582594be2a50ec44118b5b0671eda850
abybells <abbiolbello@yahoo.com>
2022-03-21 11:16:58 -0700

    7-puts_half file added

---
+++ 0x05-pointers_arrays_strings/7-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - check the code forHolberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char *str;
+
+	str = "0123456789";
+	puts_half(str);
+	return (0);
+}

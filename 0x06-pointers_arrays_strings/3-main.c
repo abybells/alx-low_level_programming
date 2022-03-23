c7f06123dcc3dabecf85bbf5d76572e329f9e58b
abybells <abbiolbello@yahoo.com>
2022-03-23 09:10:13 -0700

    3-main.c file corrected

--- 0x06-pointers_arrays_strings/3-main.c
+++ 0x06-pointers_arrays_strings/3-main.c
@@ -8,10 +8,10 @@
  */
 int main(void)
 {
-	char  s1[] = "Hello";
+	char s1[] = "Hello";
 	char s2[] = "World!";
 
-	printf("%d\n", _strncmp(s1, s2));
+	printf("%d\n", _strcmp(s1, s2));
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
 	return (0);

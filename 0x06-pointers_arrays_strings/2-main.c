4a6e68a2b2e649c9ef40053a1f03f463398f60b7
abybells <abbiolbello@yahoo.com>
2022-03-23 08:54:38 -0700

    2-main.c file updated

--- 0x06-pointers_arrays_strings/2-main.c
+++ 0x06-pointers_arrays_strings/2-main.c
@@ -12,7 +12,7 @@ int main(void)
 	char *ptr;
 	int i;
 
-	for (i = 0, i < 98 - 1; i++)
+	for (i = 0; i < 98 - 1; i++)
 	{
 		s1[i] = '*';
 	}
@@ -20,7 +20,7 @@ int main(void)
 	printf("%s\n", s1);
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
 	printf("%s\n", s1);
-	*printf("%s", ptr);
+	printf("%s", ptr);
 	ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
 	printf("%s", s1);
 	printf("%s", ptr);

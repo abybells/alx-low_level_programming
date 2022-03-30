35aae9b5af96fe315f20fd450943abebc22882ae
abybells <abbiolbello@yahoo.com>
2022-03-30 10:24:39 -0700

    100-palindrome added

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -12,7 +12,7 @@ int _strlen_recursion(char *s)
 }
 /**
  * is_palind_recursive - checks if two chars of a string are equal
- * @n: string to be checked
+ * @s: string to be checked
  * @i: first index
  * @j: last index
  * Return: 1 if equal, else 0
@@ -39,5 +39,5 @@ int is_palindrome(char *s)
 	len = _strlen_recursion(s);
 	if (len == 0 || *s != s[len - 1])
 		return (0);
-	return (is_palind_recursive(s, 0, len -1));
+	return (is_palind_recursive(s, 0, len - 1));
 }

47535a6910ed638c062e54c53438e7f8d9cf4cea
abybells <abbiolbello@yahoo.com>
2022-04-04 11:18:49 -0700

    2-str_concat.c file updated

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,6 +10,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *strDup;
 	int i, j;
+
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
@@ -31,7 +32,8 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 	{
 		strDup[i] = s2[j];
-		i++; j++;
+		i++; 
+		j++;
 	}
 	strDup[i] = '\0';
 	return (strDup);

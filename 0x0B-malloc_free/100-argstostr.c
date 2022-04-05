e00761eabeb972eb357aef0ebd0400d3ac24516a
abybells <abbiolbello@yahoo.com>
2022-04-05 05:05:51 -0700

    args file added

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,42 @@
+#include "main.h"
+#include <stdlb.h>
+/**
+ * *argstostr - concatenates all arguments to the program
+ * @ac: argument count
+ * @av: pointer to arguments
+ * Return: pointer to new space in memory or null
+ */
+char *argstostr(int ac, int av)
+{
+	char *strDup;
+	int i, j, k, size;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	size = 0;
+	/* count the number of chars in each string */
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+			size++;
+		size++;
+	}
+	size++;
+	/* allocate memory for total number of chars and
+	 * new line for each word
+	 */
+	strDup = malloc(sizof(char) * size);
+	if (strDup == NULL)
+		return (NULL);
+	k = 0;
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j] != '\0'; j++)
+		{
+			strDup[k++] = av[i][j];
+		}
+		strDup[k++] = '\n';
+	}
+	strDup[k] = '\0';
+	return (strDup);
+}

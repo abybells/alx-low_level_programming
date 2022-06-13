5bc5b85e0c8c6cf8a71dea18aa6820f6dcbd0d91
abybells <99332371+abybells@users.noreply.github.com>
2022-06-13 18:22:34 +0100

    Create cops.mul.c

---
+++ 0x18-dynamic_libraries/cops.mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ *mul - multiplies two integers
+ *@a: integer entered
+ *@b: integer entered
+ *Return: multiplication result of a and b
+ */
+int mul(int a, int b)
+{
+  return (a * b);
+}

60ac1547c80cef553b65749b095855b95c9ea85f
abybells <99332371+abybells@users.noreply.github.com>
2022-06-13 20:38:59 +0100

    Update 101-make_me_win.sh

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget https://github.com/abybells/alx-low_level_programming/blob/master/0x18-dynamic_libraries/libdynamic.so
-export LD_PRELOAD=./inject.so
+wget -P https://github.com/abybells/alx-low_level_programming/blob/master/0x18-dynamic_libraries/inject.so
+export LD_PRELOAD=/tmp/inject.so

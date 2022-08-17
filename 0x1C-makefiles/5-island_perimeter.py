286a05b66566f97ce4958765cb6002666f35e5fa
abybells <abbiolbello@yahoo.com>
2022-08-17 07:53:54 -0700

    island_perimeter

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,7 +7,7 @@ Island Perimeter
 def island_perimeter(grid):
     """function that returns the perimeter of the island described in grid """
     count = 0
-    for i in range90, len(grid)):
+    for i in range(0, len(grid)):
         for j in range(0, len(grid[i])):
             if grid[i][j] == 1:
                 count += 4
@@ -17,6 +17,6 @@ def island_perimeter(grid):
                     count -= 1
                 if j > 0 and grid[i][j - 1] == 1:
                     count -= 1
-                if j < len(grid[i]) - 1 and grid[i][j + 1] == :
+                if j < len(grid[i]) - 1 and grid[i][j + 1] == 1:
                     count -= 1
     return count

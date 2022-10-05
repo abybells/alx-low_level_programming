fd07f1e8078dbe9c4c35e0aca97c5f0a41be4002
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-05 18:48:01 +0100

    Update 0-linear.c

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,8 +1,9 @@
 #include "search_algos.h"
+
 /**
 * linear_search - a function that searches for a value in an
-*array of integers using the Linear search algorithm
-* @array: is a pointer to the first element of the array to search in
+* array of integers using the Linear search algorithm
+* @array: is a pointer to the first element of the array to search
 * @size: is the number of elements in array
 * @value: is the value to search for
 * Return: index OR -1

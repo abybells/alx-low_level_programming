55af281c4ac1a6d929e28d1697170dc7696bffb0
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-05 16:33:28 +0100

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -12,5 +12,8 @@ int jump_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int help_binary(int *array, int value, size_t lo, size_t hi);
 void array_print(int *array, size_t lo, size_t hi);
+int interpolation_search(int *array, size_t size, int value);
+
+
 
 #endif

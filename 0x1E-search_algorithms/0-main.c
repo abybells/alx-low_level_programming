6a8dc60f9579ed4ab75c007d216e931b66f928f0
Abiola Olasunkanmi Bello <99332371+abybells@users.noreply.github.com>
2022-10-05 19:03:23 +0100

    Update 0-main.c

--- 0x1E-search_algorithms/0-main.c
+++ 0x1E-search_algorithms/0-main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stddef.h>
 #include "search_algos.h"
+#include <stdlib.h>
 
 /**
  * main - Entry point
@@ -9,13 +10,13 @@
  */
 int main(void)
 {
-    int array[] = {
-        10, 1, 42, 3, 4, 42, 6, 7, -1, 9
-    };
-    size_t size = sizeof(array) / sizeof(array[0]);
+	int array[] = {
+		10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+	};
+	size_t size = sizeof(array) / sizeof(array[0]);
 
-    printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
-    printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
-    printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
-    return (EXIT_SUCCESS);
+	printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+	printf("Found %d at index: %d\n\n", 42, linear_search(array, size, 42));
+	printf("Found %d at index: %d\n", 999, linear_search(array, size, 999));
+	return (EXIT_SUCCESS);
 }
